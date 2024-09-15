0b9468cfdb717a9d94137bfafad785d5123f3436
Victor Batista <victorbtst77@gmail.com>
2024-09-15 15:35:13 -0300

    Rotas

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { createClient } from '@libsql/client';
 import dotenv from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
 import express from 'express';
+import { routes } from './routes';
 
 dotenv.config();
 
@@ -12,8 +13,8 @@ const port = process.env.PORT || 3000;
 
 
 const client = createClient({
-  url: process.env.DATABASE_URL!,
-  authToken: process.env.DATABASE_AUTH_TOKEN,
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
 });
 
 export const db = drizzle(client);
@@ -25,6 +26,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+app.use('/api', routes);
+
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+
+
+export const routes = express.Router();
+
