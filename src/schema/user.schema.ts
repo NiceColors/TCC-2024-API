7ae453990b2bc36b62f51e32f49b0220b9594e12
Victor Batista <victorbtst77@gmail.com>
2024-09-15 16:23:28 -0300

    :sparkles: feat: User schema validation

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,5 +1,8 @@
+import type { InferSelectModel } from 'drizzle-orm';
 import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
 
 export const users = sqliteTable('users', {
     id: integer('id').primaryKey({ autoIncrement: true }),
@@ -14,3 +17,56 @@ export const users = sqliteTable('users', {
         .default(sql`CURRENT_TIMESTAMP`)
 })
 
+export const selectUserSchema = createSelectSchema(users, {
+    email: schema =>
+        schema.email.email().regex(/^([\w.%-]+@[a-z0-9.-]+\.[a-z]{2,6})*$/i),
+});
+
+export const verifyUserSchema = z.object({
+    query: selectUserSchema.pick({
+        email: true,
+    }),
+});
+
+export const deleteUserSchema = z.object({
+    body: selectUserSchema.pick({
+        email: true,
+    }),
+});
+
+export const loginSchema = z.object({
+    body: selectUserSchema.pick({
+        email: true,
+        password: true,
+    }),
+});
+
+export const addUserSchema = z.object({
+    body: selectUserSchema.pick({
+        name: true,
+        email: true,
+        password: true,
+    }),
+});
+
+export const updateUserSchema = z.object({
+    body: selectUserSchema
+        .pick({
+            name: true,
+            email: true,
+            password: true,
+        })
+        .partial(),
+});
+
+export const newUserSchema = z.object({
+    body: selectUserSchema.pick({
+        name: true,
+        email: true,
+        password: true,
+    }),
+});
+
+export type User = InferSelectModel<typeof users>;
+export type NewUser = z.infer<typeof newUserSchema>['body'];
+export type UpdateUser = z.infer<typeof updateUserSchema>['body'];
