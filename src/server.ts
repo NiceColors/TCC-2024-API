612a56f802007a4c1bd0e943ec57f9cfb014b0ee
Victor Batista <victorbtst77@gmail.com>
2024-09-15 16:13:47 -0300

    :sparkles: feat: Control access

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,13 @@ import dotenv from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
 import express from 'express';
 import { authRoutes, routes } from './routes/routes';
+import rateLimit from 'express-rate-limit';
+import cors from 'cors';
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
 
 const client = createClient({
   url: process.env.DATABASE_URL!,
@@ -21,12 +23,21 @@ export const db = drizzle(client);
 
 export const app = express();
 
+app.use(cors());
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
 
 app.use('/auth', authRoutes);
+
 app.use('/api', routes);
 
 
