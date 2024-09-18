303f007282291020aa5be523679f657fd9d29265
Victor Batista <victorbtst77@gmail.com>
2024-09-17 23:28:05 -0300

    :arrow_up: db -> libsql

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
-import { createClient } from '@libsql/client/.';
+import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 const client = createClient({
-    url: process.env.DATABASE_URL!,
-    authToken: process.env.DATABASE_AUTH_TOKEN,
-  });
-  
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN,
+});
+
 export const db = drizzle(client);
